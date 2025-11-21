ea384b4313d9fbc54761954d2749a4aecc177c5a
Aref Ghoreishee <148600819+Aref7792@users.noreply.github.com>
2025-11-21 17:13:10 -0500

    Add files via upload

---
+++ Submitted_Code/Roundabout/MM_DQN.py
@@ -0,0 +1,587 @@
+import os.path
+import os
+from tabnanny import NannyNag
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
+
+from pandas.core.nanops import nanmax
+from sympy.physics.vector.printing import params
+
+from torch import nn
+import torch
+import gymnasium as gym
+from collections import deque
+import itertools
+import numpy as np
+import random
+# import ale_py
+# from stable_baselines3.common.vec_env import VecTransposeImage
+# from stable_baselines3.common.vec_env.base_vec_env import VecEnv, VecEnvIndices, VecEnvObs, VecEnvStepReturn
+# from stable_baselines3.common.env_util import make_atari_env
+# from stable_baselines3.common.vec_env.dummy_vec_env import DummyVecEnv
+# from stable_baselines3.common.vec_env import VecFrameStack
+# from stable_baselines3.common.monitor import Monitor
+from torch.utils.tensorboard import SummaryWriter
+import numpy as np
+import torch as th
+import torch.nn as nn
+import snntorch
+
+# from pytorch_wrappers import BatchedPytorchFrameStack, PytorchLazyFrames
+import torch.nn.functional as F
+from typing import Any, Dict, List, Optional, Tuple, Type, Union
+from torch.autograd import Variable
+#from myutils.LeakyL import Leaky
+import snntorch as snn
+from snntorch import spikegen
+import highway_env
+import msgpack
+import einops
+from msgpack_numpy import patch as msgpack_numpy_patch
+msgpack_numpy_patch()
+
+device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+GAMMA=0.99
+BATCH_SIZE=32
+BUFFER_SIZE=int(5e4)
+MIN_REPLAY_SIZE=10000
+
+EPSILON_START=1.0
+EPSILON_END=0.1
+NUM_ENV=1
+EPSILON_DECAY=int(7e4)
+TARGET_UPDATE_FREQ = 100//NUM_ENV
+LR= 1e-4
+SAVE_PATH= './atari_model.pack'
+SAVE_INTERVAL= 5000
+LOG_DIR= 'logs/MM_DQN'
+LOG_INTERVAL=1000
+
+
+#dtermine the seed
+seed=0
+torch.manual_seed(seed)
+torch.cuda.manual_seed(seed)
+np.random.seed(seed)
+
+os.makedirs('training_model', exist_ok=True)
+
+config1 = {
+    "observation": {
+        "type": "LidarObservation",
+        "features": ["presence", "distance"],
+        "cells": 800,
+        "maximum_range": 60
+    },
+    "action": {"type": "DiscreteMetaAction", "target_speeds": [0, 8, 16]},
+                "incoming_vehicle_destination": None,
+                "collision_reward": -1,
+                "high_speed_reward": 0.2,
+                "right_lane_reward": 0,
+                "lane_change_reward": -0.05,
+                "screen_width": 600,
+                "screen_height": 600,
+                "centering_position": [0.5, 0.6],
+                "duration": 11,
+                "normalize_reward": True,
+}
+
+config = {
+    "observation": {
+        "type": "GrayscaleObservation",
+        "observation_shape": (128, 128),
+        "stack_size": 1,
+        "weights": [0.299, 0.587, 0.114],  # weights for RGB conversion
+        "scaling": 2,
+        "centering_position": [.5,.5]
+    },
+    "action": {"type": "DiscreteMetaAction", "target_speeds": [0, 8, 16]},
+                "incoming_vehicle_destination": None,
+                "collision_reward": -1,
+                "high_speed_reward": 0.2,
+                "right_lane_reward": 0,
+                "lane_change_reward": -0.05,
+                "screen_width": 600,
+                "screen_height": 600,
+                "centering_position": [0.5, 0.6],
+                "duration": 11,
+                "normalize_reward": True,
+}
+
+config2 = {
+    "observation": {
+        "type": "Kinematics",
+        "vehicles_count": 1,
+        "features": ["heading"],
+        "absolute": False,
+        "order": "sorted"
+    }, "action": {"type": "DiscreteMetaAction", "target_speeds": [0, 8, 16]},
+                "incoming_vehicle_destination": None,
+                "collision_reward": -1,
+                "high_speed_reward": 0.2,
+                "right_lane_reward": 0,
+                "lane_change_reward": -0.05,
+                "screen_width": 600,
+                "screen_height": 600,
+                "centering_position": [0.5, 0.6],
+                "duration": 11,
+                "normalize_reward": True,
+}
+
+
+def draw_centered_vehicle(grid, length_cells, width_cells, heading_rad, value=1):
+    """
+    Draw a rotated rectangle in the center of the grid.
+    """
+    H, W = grid.shape
+    cy, cx = (H - 1) / 2.0, (W - 1) / 2.0  # center cell coords
+
+    j = np.arange(W)[None, :]   # column indices
+    i = np.arange(H)[:, None]   # row indices
+    x = j - cx
+    y = i - cy
+
+    # Rotation
+    c, s = np.cos(heading_rad), np.sin(heading_rad)
+    x_loc =  c * x + s * y
+    y_loc = -s * x + c * y
+
+    # Half-size
+    hx = length_cells / 2.0
+    hy = width_cells  / 2.0
+
+    mask = (np.abs(x_loc) <= hx) & (np.abs(y_loc) <= hy)
+    grid[mask] = value
+    return grid
+class LidarToOccupancyGrid:
+    def __init__(self, voxel_size=0.5, x_range=(-60, 60), y_range=(-60, 60), max_distance=60):
+        self.voxel_size = voxel_size
+        self.x_min, self.x_max = x_range
+        self.y_min, self.y_max = y_range
+        self.max_distance = max_distance
+
+        self.grid_width = int((self.x_max - self.x_min) / self.voxel_size)
+        self.grid_height = int((self.y_max - self.y_min) / self.voxel_size)
+        self.occupancy_grid = np.zeros((self.grid_height + 1, self.grid_width + 1), dtype=int)
+        print(self.occupancy_grid.shape)
+
+    def process(self, obs, angles, v_vel, heading):
+        points = []
+
+        for i, ray in enumerate(obs):
+            if ray[0] > 0 and ray[0] != 1:  # presence
+                num = int((1 - abs(ray[0])) * 500)
+                distance = np.linspace(ray[0], 1, num) * self.max_distance
+
+                x = distance * np.cos(angles[i])
+                y = distance * np.sin(angles[i])
+                decay = 0.98 ** np.arange(num)
+                z = decay * (ray[1]+v_vel)
+                #z = decay * distance
+                points.append([x, y, z])
+
+        self.occupancy_grid.fill(0)
+
+        # Add ego vehicle's rectangle
+        vl = int(2 // self.voxel_size)
+        vh = int(5 // self.voxel_size)
+        cx, cy = self.grid_width // 2, self.grid_height // 2
+
+        self.occupancy_grid = draw_centered_vehicle(self.occupancy_grid, vl, vh, np.pi - heading, v_vel)
+
+
+        # Mark LiDAR points
+        for pt in points:
+            for j in range(len(pt[0])):
+                l = int(pt[0][j] // self.voxel_size)
+                k = int(pt[1][j] // self.voxel_size)
+
+                l = self.grid_width // 2 + l
+                k = self.grid_height // 2 - k
+
+                if 0 <= k < self.grid_height and 0 <= l < self.grid_width:
+                    self.occupancy_grid[l, k] =  pt[2][j]
+
+        #occ = self.occupancy_grid[79:163, 99:141]/30
+        occ = self.occupancy_grid[50:200,50:200]/ 16
+        cmap = plt.get_cmap("viridis")
+        rgbocc = np.uint8(cmap(occ)[:,:,:3]*255)
+
+
+
+        rgbocc = np.transpose(rgbocc,(2,0,1))
+
+
+
+        #occe = np.expand_dims(occ, 0)
+
+        return rgbocc
+
+lidar_converter = LidarToOccupancyGrid()
+
+
+class EncoderBlock(nn.Module):
+    def __init__(self, latent_size, num_heads, device=device):
+        super(EncoderBlock, self).__init__()
+
+        self.latent_size = latent_size
+        self.num_heads = num_heads
+        self.device = device
+
+        # Normalization layer
+        self.norm1 = nn.LayerNorm(self.latent_size)
+        self.norm2 = nn.LayerNorm(self.latent_size)
+        self.norm3 = nn.LayerNorm(self.latent_size)
+
+        self.multihead = nn.MultiheadAttention(
+            self.latent_size, self.num_heads, batch_first=True)
+
+        self.enc_MLP = nn.Sequential(
+            nn.Linear(self.latent_size, self.latent_size * 4),
+            nn.GELU(),
+            nn.Linear(self.latent_size * 4, self.latent_size))
+
+
+    def forward(self, embedded_patches1 , embedded_patches2):
+
+
+        Q = self.norm1(embedded_patches1)
+        V = self.norm2(embedded_patches2)
+        attention_out = self.multihead(Q, V, V)[0]
+        # first residual connection
+        first_added = attention_out + embedded_patches1
+
+        secondnorm_out = self.norm3(first_added)
+        ff_out = self.enc_MLP(secondnorm_out)
+
+        return ff_out + first_added
+
+
+class InputEmbedding(nn.Module):
+    def __init__(self, n_channels, device, latent_size, dim1, dim2):
+        super(InputEmbedding, self).__init__()
+        self.latent_size = latent_size
+        self.n_channels = n_channels
+        self.device = device
+        self.input_size = self.n_channels
+        self.dim1 = dim1
+        self.dim2 = dim2
+        # Linear projection
+        self.linearProjection = nn.Linear(self.input_size, self.latent_size)
+        self.pos_embedding = nn.Parameter(torch.randn(1, self.dim1, self.dim2)).to(self.device)
+
+    def forward(self, input_data):
+        input_data = input_data.to(self.device)
+
+        patches1 = einops.rearrange(input_data, 'b c (h) (w) -> b h w (c)')
+
+
+        patches2 = patches1 + self.pos_embedding.unsqueeze(-1)
+
+        patches = einops.rearrange(patches2, 'b h w (c) -> b (h w) (c)')
+        linear_projection = self.linearProjection(patches).to(self.device)
+
+        return linear_projection
+
+class Network(nn.Module):
+    def __init__(self, env1, env2, device, depths1, depths2, final_layer):
+        super().__init__()
+        self.num_actions = env1.action_space.n
+
+        self.outdim = env1.action_space.n
+        self.device = device
+        self.depths1 = depths1
+        self.depths2 = depths2
+        self.final_layer = final_layer
+        n_input_channels1 = env1.observation_space.shape[0]
+        n_input_channels2 = 3
+        self.fc11 = nn.Conv2d(n_input_channels1, depths1[0], kernel_size=5, stride=3)
+        self.fc21 = nn.ReLU()
+        self.fc31 = nn.Conv2d(depths1[0], depths1[1], kernel_size=3, stride=2)
+        self.fc41 = nn.ReLU()
+        self.fc51 = nn.Conv2d(depths1[1], depths1[2], kernel_size=3, stride=1)
+        self.fc61 = nn.ReLU()
+        self.emb1 = InputEmbedding(depths1[2], device=device, latent_size=32, dim1=18, dim2=18)
+
+        self.encLIF2 = snntorch.Leaky(1, .5)
+        self.fc12 = nn.Conv2d(n_input_channels2, depths2[0], kernel_size=7, stride=3)
+        self.fc22 = nn.ReLU()
+        self.fc32 = nn.Conv2d(depths2[0], depths2[1], kernel_size=5, stride=3)
+        self.fc42 = nn.ReLU()
+        self.fc52 = nn.Conv2d(depths2[1], depths2[2], kernel_size=3, stride=1)
+        self.fc62 = nn.ReLU()
+        self.fc72 = nn.Conv2d(depths2[1], depths2[2], kernel_size=3, stride=1)
+        self.fc82 = nn.ReLU()
+        self.emb2 = InputEmbedding(depths2[1], device=device, latent_size=32, dim1=13, dim2=13)
+
+        self.cross = EncoderBlock(32, 8, device=device)
+
+        self.fc7 = nn.Flatten()
+        self.fc8 = nn.Linear(5408, final_layer)
+        self.fc9 = nn.ReLU()
+        self.fc10 = nn.Linear(final_layer, self.outdim)
+
+    def forward(self, x1, x2):
+        x1 = x1 / 255
+
+        x2 = x2 / 255
+
+        V = self.emb1(self.fc61(self.fc51(self.fc41(self.fc31(self.fc21(self.fc11(x1)))))))
+
+        Q = self.emb2(self.fc62(self.fc52(self.fc42(self.fc32(self.fc22(self.fc12(x2)))))))
+
+
+
+        cros = self.cross(Q, V)
+        re_cros = einops.rearrange(cros, 'b (h w) c -> b (c) h w', h=13)
+
+        out_Att = self.fc10(self.fc9(self.fc8(self.fc7(re_cros))))
+
+
+        return out_Att
+
+    def act(self, obses, epsilon):
+        obses_t1 = torch.as_tensor(obses[0], dtype=torch.float32, device=self.device).unsqueeze(0)
+        obses_t2 = torch.as_tensor(obses[1], dtype=torch.float32, device=self.device).unsqueeze(0)
+        q_values = self(obses_t1, obses_t2)
+        max_q_indices = torch.argmax(q_values, dim=1)
+        actions = max_q_indices.detach().item()
+
+
+
+        rnd_sample = random.random()
+        if rnd_sample <= epsilon:
+            actions = random.randint(0, self.num_actions-1)
+
+        return actions
+
+    def compute_loss(self, transitions, target_net):
+
+        obses1 = np.asarray([t[0][0] for t in transitions])
+        obses2 = np.asarray([t[0][1] for t in transitions])
+        actions = np.asarray([t[1] for t in transitions])
+        rews = np.asarray([t[2] for t in transitions])
+        dones = np.asarray([t[3] for t in transitions])
+        new_obses1 = np.asarray([t[4][0] for t in transitions])
+        new_obses2 = np.asarray([t[4][1] for t in transitions])
+        obses_t1 = torch.as_tensor(obses1, dtype=torch.float32, device=self.device)
+        obses_t2 = torch.as_tensor(obses2, dtype=torch.float32, device=self.device)
+        actions_t = torch.as_tensor(actions, dtype=torch.int64, device=self.device).unsqueeze(-1)
+        rews_t = torch.as_tensor(rews, dtype=torch.float32, device=self.device).unsqueeze(-1)
+        dones_t = torch.as_tensor(dones, dtype=torch.float32, device=self.device).unsqueeze(-1)
+        new_obses_t1 = torch.as_tensor(new_obses1, dtype=torch.float32, device=self.device)
+        new_obses_t2 = torch.as_tensor(new_obses2, dtype=torch.float32, device=self.device)
+
+        # Compute Targets
+        # targets = r + gamma * target q vals * (1 - dones)
+        target_q_values = target_net(new_obses_t1, new_obses_t2)
+        max_target_q_values = target_q_values.max(dim=1, keepdim=True)[0]
+        targets = rews_t + GAMMA * (1 - dones_t) * max_target_q_values
+
+        # Compute Loss
+        q_values = self(obses_t1, obses_t2)
+        action_q_values = torch.gather(input=q_values, dim=1, index=actions_t)
+        loss = nn.functional.smooth_l1_loss(action_q_values, targets)
+        return loss
+
+    def save(self, epoch):
+        print('model saved')
+        th.save(self.state_dict(), 'training_model/MM_DQN_R_' + str(epoch) + '.pth')
+
+    def load(self, epoch):
+        print('load model')
+        self.load_state_dict(th.load('training_model/MM_DQN_R_' + str(epoch) + '.pth'))
+
+
+env = gym.make("roundabout-v0", render_mode="rgb_array", config=config)
+env2= gym.make("roundabout-v0", render_mode="rgb_array", config=config1)
+env3= gym.make("roundabout-v0", render_mode="rgb_array", config=config2)
+replay_buffer = deque(maxlen=BUFFER_SIZE)
+epinfos_buffer = deque([], maxlen=100)
+
+tenv = gym.make("roundabout-v0", render_mode="rgb_array", config=config)
+tenv2= gym.make("roundabout-v0", render_mode="rgb_array", config=config1)
+tenv3= gym.make("roundabout-v0", render_mode="rgb_array", config=config2)
+
+
+
+episode_count = 0
+summary_writer = SummaryWriter(LOG_DIR)
+
+#seting networks' architecture
+online_net = Network(env,env2, device=device, depths1=(8, 16,16), depths2=(8, 16,16), final_layer=512)
+target_net = Network(env,env2, device=device, depths1=(8, 16,16), depths2=(8, 16,16), final_layer=512)
+
+online_net= online_net.to(device)
+target_net=target_net.to(device)
+
+target_net.load_state_dict(online_net.state_dict())
+optimizer = torch.optim.Adam(online_net.parameters(), lr=LR)
+
+
+rews_buffer_=[]
+
+
+# Main Training Loop
+h_seed = np.random.randint(0, 2**63)
+obs1, infos = env.reset(seed=h_seed)
+obs2, _ = env2.reset(seed=h_seed)
+obs3, _ = env3.reset(seed=h_seed)
+num_rays = config1["observation"]["cells"]
+angle_range = [0, -2 * np.pi]
+angles = np.linspace(angle_range[0], angle_range[1], num_rays)
+max_distance = 60
+heading = obs3[0][0]
+v_vel=infos['speed']
+
+occupancy_grid = lidar_converter.process(obs2, angles, v_vel, heading)
+obsp = occupancy_grid
+
+obs = [obs1, obsp]
+
+save_test_reward = []
+save_test_reward_steps = []
+dter = [0,0,0,0]
+
+t = 0
+
+
+for step in range(int(1e5)):
+
+    epsilon = np.interp(step * NUM_ENV, [0, EPSILON_DECAY], [EPSILON_START, EPSILON_END])
+    rnd_sample = random.random()
+    action = online_net.act(obs, epsilon)
+    new_obs1, rew, done, termin, infos = env.step(action)
+    new_obs2, rew2, done2, _, infos2 = env2.step(action)
+    new_obs3, _, _, _, _ = env3.step(action)
+    heading = new_obs3[0][0]
+    v_vel = infos['speed']
+    occupancy_grid = lidar_converter.process(new_obs2, angles, v_vel, heading)
+    obsp = occupancy_grid
+
+    new_obs = [new_obs1, obsp]
+
+
+
+    i=0
+    rews_buffer_.append(rew)
+    transition = (obs, action, rew, done, new_obs)
+    replay_buffer.append(transition)
+
+    obs = new_obs
+    t = t+1
+
+
+
+
+
+    if done or termin:
+
+
+
+        eprew = sum(rews_buffer_)
+        eplen = len(rews_buffer_)
+
+        epinfo = {"r": round(eprew, 6), "l": eplen}
+        epinfos_buffer.append(epinfo)
+
+
+        rews_buffer_=[]
+        episode_count+=1
+        h_seed = np.random.randint(0, 2 ** 63)
+
+        obs1, infos = env.reset(seed=h_seed)
+        obs2, _ = env2.reset(seed=h_seed)
+        obs3, _ = env3.reset(seed=h_seed)
+        v_vel = infos['speed']
+        heading = obs3[0][0]
+        occupancy_grid = lidar_converter.process(obs2, angles, v_vel, heading)
+        obsp = occupancy_grid
+        obs = [obs1, obsp]
+        t = 0
+
+
+
+    if len(replay_buffer) >= 1000:
+
+
+        transitions = random.sample(replay_buffer, BATCH_SIZE)
+        loss = online_net.compute_loss(transitions, target_net)
+
+    # Gradient Descent
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    # Update Target Net
+    if step % TARGET_UPDATE_FREQ == 0:
+        target_net.load_state_dict(online_net.state_dict())
+
+    # Logging
+    if step % LOG_INTERVAL == 0:
+
+        rew_mean = np.mean([e['r'] for e in epinfos_buffer]) or 0
+        len_mean = np.mean([e['l'] for e in epinfos_buffer]) or 0
+
+
+
+
+
+        if step==0:
+            rew_mean = 0
+            len_mean = 0
+        print()
+        print('Step:', step)
+        print('Avg Rew:', rew_mean)
+        print('Avg Ep len:', len_mean)
+        print('Epizode:', episode_count)
+
+        summary_writer.add_scalar('AvgRew', rew_mean, global_step=step)
+        summary_writer.add_scalar('AvgLen', len_mean, global_step=step)
+
+
+
+    if step % SAVE_INTERVAL==0:
+
+        test_reward_sum = 0
+
+
+
+        for ij in range(20):
+            tseed = np.random.randint(0, 2 ** 63)
+            o1, tinfo = tenv.reset(seed=tseed)
+            o2, _ = tenv2.reset(seed=tseed)
+            o3, _ = tenv3.reset(seed=tseed)
+
+            v_velt = tinfo['speed']
+            headingt = o3[0][0]
+            occupancy_grid = lidar_converter.process(o2, angles, v_velt, headingt)
+            opt = occupancy_grid
+            obst = [o1, opt]
+            d = False
+            ep_len = 0
+            ep_ret = 0
+            ttermin = 0
+
+            while not (d or (ttermin)):
+                a = online_net.act(obst, 0)
+                tnew_obs1, trew, d, ttermin, tinfos = tenv.step(a)
+                tnew_obs2, trew2, tdone2, _, tinfos2 = tenv2.step(a)
+                tnew_obs3, _, _, _, _ = tenv3.step(a)
+                theading = tnew_obs3[0][0]
+                tv_vel = tinfos['speed']
+                occupancy_grid = lidar_converter.process(tnew_obs2, angles, tv_vel, theading)
+                tobsp = occupancy_grid
+                tnew_obs = [tnew_obs1, tobsp]
+                ep_ret += trew
+                ep_len += 1
+                obst = tnew_obs
+            test_reward_sum += ep_ret
+        test_reward_sum = test_reward_sum / 20
+        summary_writer.add_scalar('TestRew', test_reward_sum, global_step=step)
+        print('TestRew', test_reward_sum)
+        print('Saving...')
+        online_net.save(step)
